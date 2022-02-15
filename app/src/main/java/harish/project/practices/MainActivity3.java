b2ac6edc7c0848725fedba840449c23d5606cb41
Harish <harishchoudhary6258@gmail.com>
2022-02-15 16:02:59 +0530

    first project

---
+++ app/src/main/java/harish/project/practices/MainActivity3.java
@@ -0,0 +1,37 @@
+package harish.project.practices;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity3 extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        /*find id of text view id*/
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,
+                Toast.LENGTH_SHORT);
+        toast.show();
+        /* Shows a Toast when the TOAST button is clicked.*/
+    }
+    /*
+     increases the number in the TextView when the COUNT button is clicked.
+     */
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+}
